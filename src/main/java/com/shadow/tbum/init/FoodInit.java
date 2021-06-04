a237f0748df29547312868ca3a7e724bec49c932
PurplePineaapple <clovistardif@gmail.com>
2021-06-04 18:49:55 -0500

    Added Fruit

--- src/main/java/com/shadow/tbum/init/FoodInit.java
+++ src/main/java/com/shadow/tbum/init/FoodInit.java
@@ -1,11 +1,8 @@
 package com.shadow.tbum.init;
 
-import com.shadow.tbum.UtilitiesMod;
 import net.minecraft.item.Food;
-import net.minecraft.item.Item;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
-import net.minecraftforge.fml.RegistryObject;
 
 public class FoodInit {
     public static final Food CARMEL_APPLE = new Food.Builder()
@@ -17,6 +14,12 @@ public class FoodInit {
                     .nutrition(4).saturationMod(0.4f).alwaysEat().build();
 
     public static final Food HAMBURGER = new Food.Builder().nutrition(30).saturationMod(50f).alwaysEat().build();
-
     public static final Food LETTUCE = new Food.Builder().nutrition(2).saturationMod(1f).alwaysEat().fast().build();
+
+    public static final Food BLUEBERRY = new Food.Builder().nutrition(2).saturationMod(0.2F).build();
+    public static final Food STRAWBERRY = new Food.Builder().nutrition(2).saturationMod(0.275F).build();
+    public static final Food BANANA = new Food.Builder().nutrition(2).saturationMod(0.3f).build();
+    public static final Food CHERRY = new Food.Builder().nutrition(2).saturationMod(0.275f).build();
+    public static final Food PEAR = new Food.Builder().nutrition(3).saturationMod(0.375f).build();
+    public static final Food ORANGE = new Food.Builder().nutrition(3).saturationMod(0.375f).build();
 }
--- src/main/java/com/shadow/tbum/init/ItemInit.java
+++ src/main/java/com/shadow/tbum/init/ItemInit.java
@@ -1,10 +1,7 @@
 package com.shadow.tbum.init;
 
 import com.shadow.tbum.UtilitiesMod;
-import net.minecraft.item.Food;
 import net.minecraft.item.Item;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -22,5 +19,18 @@ public class ItemInit {
             new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.HAMBURGER)));
     public static final RegistryObject<Item> LETTUCE = ITEMS.register("lettuce", () ->
             new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.LETTUCE)));
+
+    public static final RegistryObject<Item> BLUEBERRY = ITEMS.register("blueberry", () ->
+            new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.BLUEBERRY)));
+    public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry", () ->
+            new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.STRAWBERRY)));
+    public static final RegistryObject<Item> BANANA = ITEMS.register("banana", () ->
+            new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.BANANA)));
+    public static final RegistryObject<Item> CHERRY = ITEMS.register("cherry", () ->
+            new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.CHERRY)));
+    public static final RegistryObject<Item> PEAR = ITEMS.register("pear", () ->
+            new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.PEAR)));
+    public static final RegistryObject<Item> ORANGE = ITEMS.register("orange", () ->
+            new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.ORANGE)));
 
 }
