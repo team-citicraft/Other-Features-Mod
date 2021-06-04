023490dec18973f43e897f40bab35857c45f19c0
PurplePineaapple <clovistardif@gmail.com>
2021-06-04 18:31:29 -0500

    Added Lettuce and Hamberger lang, fixed food init

--- src/main/java/com/shadow/tbum/init/ItemInit.java
+++ src/main/java/com/shadow/tbum/init/ItemInit.java
@@ -18,7 +18,7 @@ public class ItemInit {
             new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.CARMEL_APPLE)));
     public static final RegistryObject<Item> CARMEL = ITEMS.register("carmel", () ->
             new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.CARMEL)));
-    public static final RegistryObject<Item> HAMBURGER = ITEMS.register("HAMBURGER", () ->
+    public static final RegistryObject<Item> HAMBURGER = ITEMS.register("hamburger", () ->
             new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.HAMBURGER)));
     public static final RegistryObject<Item> LETTUCE = ITEMS.register("lettuce", () ->
             new Item(new Item.Properties().tab(UtilitiesMod.TAB).food(FoodInit.LETTUCE)));
